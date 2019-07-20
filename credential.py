d684a97a8d56411e43620401dcfa05165a6eef0a
mi-ano <ryanmiano68@gmail.com>
2019-07-20 13:24:54 +0300

    added texts

---
+++ credential.py
@@ -0,0 +1,35 @@
+class Credentials:
+    '''
+    class that generates new credentials 
+    '''
+
+    credentials_list = [] 
+
+    def __init__(self,account_name,password):
+        '''
+        init method to create instances of the credentials class
+        '''
+        self.account_name = account_name
+        self.password = password
+    
+    def save_credentials(self):
+        '''
+        save_user method to save new users into the list
+        '''
+        Credentials.credentials_list.append(self)
+    
+    @classmethod
+    def delete_credentials(cls,account_name,password):
+        '''
+        delete method to delete saved user from list
+        '''
+        for Credentials in cls.credentials_list:
+          if Credentials.account_name == account_name:
+            return Credentials
+    
+    @classmethod
+    def display_credentials(cls):
+        '''
+        method that returns the user list
+        '''
+        return cls.credentials_list
--- password.py
+++
@@ -1,6 +0,0 @@
-class Password:
-    """
-    Class that generates new instances of password
-    """
-
-    pass
