ca44a3e989889ab27c7d187623cb12bebe288ce8
mi-ano <ryanmiano68@gmail.com>
2019-07-20 13:53:23 +0300

    added texts

--- user.py
+++ user.py
@@ -1,35 +1,48 @@
-class Credentials:
-    '''
-    class that generates new credentials intsances
-    '''
+from credentials import Credentials
+class User:
+    """
+    Class that generates new users
+    """
+    user_list = []
 
-    credentials_list = [] 
+    def __init__(self,username,password):
+      self.username = username
+      self.password = password
 
-    def __init__(self,account_name,password):
-        '''
-        init method to create instances of the credentials class
-        '''
-        self.account_name = account_name
-        self.password = password
-    
-    def save_credentials(self):
+    def save_user(self):
         '''
         save_user method to save new users into the list
         '''
-        Credentials.credentials_list.append(self)
-    
-    @classmethod
-    def delete_credentials(cls,account_name,password):
+        User.user_list.append(self)
+
+    def delete_user(self):
         '''
         delete method to delete saved user from list
         '''
-        for Credentials in cls.credentials_list:
-          if Credentials.account_name == account_name:
-            return Credentials
-    
+        User.user_list.remove(self)
+
     @classmethod
-    def display_credentials(cls):
+    def display_users(cls):
         '''
         method that returns the user list
         '''
-        return cls.credentials_list
+        return cls.user_list      
+
+    @classmethod
+    def user_exist(cls,password):
+        '''
+        method to check if a user and
+        their details exist
+        '''
+        for user in cls.user_list:
+            if  user.password == password:
+                return True
+        return False    
+    @classmethod
+    def find_account(cls,password):
+        '''
+        method that finds an account by its name
+        '''
+        for user in cls.user_list:
+            if user.password == password:
+                return user    
